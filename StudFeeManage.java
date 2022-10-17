532cd9c0f2448e0cbba007a6d3216528405cca1a
dhanusrimj <114556259+dhanusrimj@users.noreply.github.com>
2022-10-17 17:25:23 +0530

    Add files via upload
    Final

--- StudFeeManage.java
+++ StudFeeManage.java
@@ -7,7 +7,11 @@ public class StudFeeManage {
 	public static void main(String[] args){
 		int balance=0;
 		Map<String,Integer>names=new LinkedHashMap<>();
+		
 		Map<String,Integer>year=new LinkedHashMap<>();
+		Map<String,String>branch=new LinkedHashMap<>();
+		Map<String,String>dept=new LinkedHashMap<>();
+		
 		names.put("Dhanu", 10000);
 		names.put("Sri", 20000);
 		names.put("Susee", 10000);
@@ -23,41 +27,62 @@ public class StudFeeManage {
 		year.put("Kaviya", 3);
 		year.put("Kalees",1);
 		year.put("Ashwin", 1);
+		
+		branch.put("Dhanu", "ECE");
+		branch.put("Sri","EEE");
+		branch.put("Susee","CSE");
+		branch.put("Dharan","MECH");
+		branch.put("Kaviya", "EEE");
+		branch.put("Kalees","MECH");
+		branch.put("Ashwin", "IT");
 
 		
-		System.out.println("-------------------Welcome's you----------------------");
-		System.out.println("-------------------ANNAMALAI UNIVERSITY------------------");
-		System.out.println("\n"+ "---------------Student's name with balance-------------");
+		dept.put("Dhanu", "BE");
+		dept.put("Sri","BE");
+		dept.put("Susee","BE");
+		dept.put("Dharan","BE");
+		dept.put("Kaviya", "BE");
+		dept.put("Kalees","BE");
+		dept.put("Ashwin", "BE");
+
 		
-		System.out.println("Name \t Fees \t Year");
+		
+		System.out.println("-------------------ANNAMALAI UNIVERSITY------------------");
+		System.out.println("----------------------Welcome's you-------------------------");
+		System.out.println("------------Bachelor of Engineering and Technology----------");
+		System.out.println("\n"+ "-------------Student's name with Pending fees-----------");
+		
+		System.out.println("Names \t Fees \t Year \t Branch\t\tDepatment");
 		System.out.println("----------------------------------------------------------");
 		for (String name: names.keySet()) {
 		    String key = name.toString();
 		    String value = names.get(name).toString();
-		    System.out.println(key +"\t"+ value+"\t"+year.get(name));
+		    System.out.println(key +"\t"+ value+"\t"+year.get(name)+"\t"+branch.get(name)+"\t"+"\t"+dept.get(name));
 		    
 		}
 		System.out.println("\n"+"------------------------------------------------------");
-		
+
 		
 		Map<String,Integer>fees=new LinkedHashMap<>();
 		fees.put("1st ", 20000);
 		fees.put("2nd ", 20000);
 		fees.put("3rd ", 20000);
-		System.out.println("\n"+"------------------Term Fee Details------------------");
+		System.out.println("\n"+"------------------Term Fees Detail------------------");
 		System.out.println("Term \t\t Fees ");
 		System.out.println("---------------------------------------------------------");
 		for (String name: fees.keySet()) {
 		    String key = name.toString();
 		    String value = fees.get(name).toString();
-		    System.out.println(key +"\t"+ value);
+		    System.out.println(key +"\t\t"+ value);
 		}
-
+		
 		Scanner sc=new Scanner(System.in);
 		while(true) {
 			System.out.println("\n"+"---------------------********-----------------------");
 			System.out.println("Enter the Name: ");
+//			System.err.println("***Starting letter must be in Caps***");
 			String name = sc.next();
+			
 			System.out.println("Enter the Amount: ");
 			int amount=sc.nextInt();
 			if(names.get(name)>= amount) {
@@ -68,7 +93,7 @@ public class StudFeeManage {
 				
 				
 			}else {
-				System.out.println("-------------*Payment unsuccessfull*--------------");
+				System.err.println("-------------***Payment unsuccessfull***--------------");
 			}
 		}
 		
